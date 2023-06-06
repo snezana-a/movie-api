27fa13e51c7dc4aa89d63a1401ed9f2f094805b6
Snezana <snezanaanastasova99@gmail.com>
2023-06-06 16:32:34 +0200

    Add repositories

---
+++ src/main/java/com/movie/lib/repositories/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.movie.lib.repositories;
+
+import com.movie.lib.models.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
