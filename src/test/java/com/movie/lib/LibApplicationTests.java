38c51a95acd5ec9510704661e074b4cdfd49b928
Snezana <snezanaanastasova99@gmail.com>
2023-06-06 14:39:56 +0200

    Initial commit

---
+++ src/test/java/com/movie/lib/LibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
