16949742ec956b5f5a39d05ac7d30b3774bd3ecf
Snezana <snezanaanastasova99@gmail.com>
2023-06-06 16:32:17 +0200

    Add controllers

---
+++ src/main/java/com/movie/lib/controllers/ReviewController.java
@@ -0,0 +1,28 @@
+package com.movie.lib.controllers;
+
+import com.movie.lib.models.Review;
+import com.movie.lib.services.ReviewService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+}
